35c3ddcc79594612fbb4ee254a8b6bec7313f1fe
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2025-02-12 18:33:32 +0100

    initial commit

---
+++ skillswap/src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw",
+    }}>
+      Hello world!
+    </div>
+  );
+}
